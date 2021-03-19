2f4067834dbe24eea90b281a6674bb85f7d534c3
manupachauri1023 <80909858+manupachauri1023@users.noreply.github.com>
2021-03-18 23:21:14 -0700

    Add files via upload

---
+++ manu leap year.py
@@ -0,0 +1,11 @@
+year = int(input("enter the year")
+    if year%4==0 and year % 100 !=0:
+        print("it is a leap year")
+    elif year % 100 ==0:
+        print("it is not leap year")
+    elif year % 400 ==0:
+        print("it is a leap year")
+    else:
+        print("it is not a leap year)
+           
+        
